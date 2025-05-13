b1659498a7cbf856d47fb2c895a9d1d9a694464f
Fruja Catalin <catalin.fruja@student.upt.ro>
2025-05-13 15:12:33 +0300

    z

--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Progress</h1>;
+  return (
+  <div className="bg-[#2a2a36] text-[#e9c703] text-4xl font-extrabold text-center p-10 rounded-2xl shadow-lg">
+  Tailwind funcționează! 🎉
+</div>
+
+  );
 }
